68cc3859ca9e3450e6a72ff4bd9978d63a3f6dff
ThanhTumg <thanhtumg.2510@gmail.com>
2024-05-08 21:22:12 +0700

    smartFanSetting-Slider

--- src/screen/SmartFanSetting.js
+++ src/screen/SmartFanSetting.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ScrollView, Text, TouchableOpacity, View, } from "react-native";
 import LottieView from "lottie-react-native";
-// import Slider from "@react-native-community/slider";
+import Slider from "@react-native-community/slider";
 // import { Slider } from '@rneui/themed';
 // import { Slider } from 'react-native-awesome-slider';
 import { Icon } from "react-native-elements";
@@ -51,14 +51,14 @@ function SmartFanSetting() {
         >
           Speed
         </Text>
-        {/* <Slider
+        <Slider
           style={{ width: 300, marginTop: 40 }}
           minimumValue={1}
           maximumValue={10}
           minimumTrackTintColor="#659A6E"
           maximumTrackTintColor="#DFE1E2"
           thumbImage={require("../../assets/images/thumb.png")}
-        /> */}
+        />
         <Text
           style={{ fontFamily: "Poppins-Regular", fontSize: 24, marginTop: 50 }}
         >
